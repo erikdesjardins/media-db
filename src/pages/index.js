21bcf6ab5033dbc763ff69e6cf2501d490c14d3b
Erik <erikdesjardins@users.noreply.github.com>
2016-01-10 16:44:05 -0500

    A few more routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 render((
     <Router history={browserHistory}>
         <Route path="/" component={}>
+			<IndexRoute component={}/>
             <Route path="items" component={}>
                 <Route path="items/:id" component={}/>
             </Route>
             <Route path="items/by/:type/:query" component={}>
-                <Route path="items/by/:type/:query/:id"/>
+                <Route path="items/by/:type/:query/:id" component={}/>
             </Route>
             <Route path="item/:id" component={}/>
             <Route path="tabs" component={}/>
+            <Route path="storage" component={}/>
             <Route path="*" component={}/>
         </Route>
     </Router>
