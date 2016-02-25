1fd37a473c3a427de9e02b0767ec85ec41822c8d
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2016-02-25 14:39:25 -0500

    add nyan progress plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const { join } = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const autoprefixer = require('autoprefixer');
+const NyanProgressPlugin = require('nyan-progress-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -27,7 +28,8 @@ module.exports = {
 	},
 	plugins: [
 		new webpack.optimize.UglifyJsPlugin(),
-		new ExtractTextPlugin('[name].bundle.css')
+		new ExtractTextPlugin('[name].bundle.css'),
+		new NyanProgressPlugin()
 	],
 	postcss() {
 		return [autoprefixer];
