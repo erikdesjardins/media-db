11c8b2d60d3ae7c0c49fe1ba3b61a0a6d21e7580
Erik <erikdesjardins@users.noreply.github.com>
2016-03-03 00:52:55 -0500

    more linting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 render((
-    <Router history={browserHistory}>
-        <Route path="/" component={null}>
+	<Router history={browserHistory}>
+		<Route path="/" component={null}>
 			<IndexRoute component={null}/>
-            <Route path="items" component={null}>
-                <Route path="items/:id" component={null}/>
-            </Route>
-            <Route path="items/by/:type/:query" component={null}>
-                <Route path="items/by/:type/:query/:id" component={null}/>
-            </Route>
-            <Route path="item/:id" component={null}/>
+			<Route path="items" component={null}>
+				<Route path="items/:id" component={null}/>
+			</Route>
+			<Route path="items/by/:type/:query" component={null}>
+				<Route path="items/by/:type/:query/:id" component={null}/>
+			</Route>
+			<Route path="item/:id" component={null}/>
 			<Route path="random" component={null}/>
-            <Route path="tabs" component={null}/>
-            <Route path="storage" component={null}/>
-            <Route path="*" component={null}/>
-        </Route>
-    </Router>
+			<Route path="tabs" component={null}/>
+			<Route path="storage" component={null}/>
+			<Route path="*" component={null}/>
+		</Route>
+	</Router>
 ), document.body);
