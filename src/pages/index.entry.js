d776c4c43bf586bb1e02894a0e48a69e1865c580
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2016-03-09 11:41:04 -0500

    port in build infrastructure from global-mediakeys

--- src/pages/index.entry.js
+++ src/pages/index.entry.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { IndexRoute, Route, Router, browserHistory } from 'react-router';
 
 render((
 	<Router history={browserHistory}>
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,34 +1,30 @@
-import webpack from 'webpack';
-import { join } from 'path';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import autoprefixer from 'autoprefixer';
+import InertEntryPlugin from 'inert-entry-webpack-plugin';
 import NyanProgressPlugin from 'nyan-progress-webpack-plugin';
+import autoprefixer from 'autoprefixer';
+import { join } from 'path';
 
-module.exports = {
-	entry: {
-		background: './src/pages/background/background.js',
-		content: './src/pages/content/content.js',
-		popup: './src/pages/popup/popup.js',
-		main: './src/pages/index.js'
-	},
+export default {
+	entry: 'extricate!interpolate!./src/manifest.json',
+	bail: process.env.NODE_ENV === 'production',
 	output: {
 		path: join(__dirname, 'dist'),
-		filename: '[name].bundle.js'
+		filename: 'manifest.json'
 	},
 	resolve: {
 		extensions: ['', '.js', '.jsx']
 	},
 	module: {
 		loaders: [
+			{ test: /\.entry\.js$/, loaders: ['spawn?name=[name].js', 'babel'] },
 			{ test: /\.js$/, loader: 'babel' },
 			{ test: /\.jsx$/, loaders: ['babel', 'react-map-styles'] },
-			{ test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css', 'postcss') },
-			{ test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css', 'postcss', 'sass') }
+			{ test: /\.scss$/, loaders: ['file?name=[name].css', 'extricate?resolve=\\.js$', 'css', 'postcss', 'sass'] },
+			{ test: /\.html$/, loaders: ['file?name=[name].[ext]', 'extricate', 'html?attrs=link:href script:src'] },
+			{ test: /\.png$/, loader: 'file?name=[name].[ext]' }
 		]
 	},
 	plugins: [
-		new webpack.optimize.UglifyJsPlugin(),
-		new ExtractTextPlugin('[name].bundle.css'),
+		new InertEntryPlugin(),
 		new NyanProgressPlugin()
 	],
 	postcss() {
