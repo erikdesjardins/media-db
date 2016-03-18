1d50a8015a260448496f44a8a9d683088a2b78de
Erik <erikdesjardins@users.noreply.github.com>
2016-03-17 23:57:02 -0400

    convert deploy script to ES6 syntax _actually_ no more whole-file `eslint-disable`, yay at least in my code

--- deploy.js
+++ deploy.js
@@ -1,20 +1,18 @@
-/* eslint-disable */
+/* eslint-disable no-console */
 
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
-const deploy = require('chrome-extension-deploy');
+import fs from 'fs';
+import path from 'path';
+import deploy from 'chrome-extension-deploy';
 
 deploy({
 	clientId: process.env.CLIENT_ID,
 	clientSecret: process.env.CLIENT_SECRET,
 	refreshToken: process.env.REFRESH_TOKEN,
 	id: 'INSERT ID HERE',
-	zip: fs.readFileSync(path.join(__dirname, 'dist/media-db.zip'))
-}).then(function() {
+	zip: fs.readFileSync(path.join(__dirname, 'dist/media-db.zip')),
+}).then(() => {
 	console.log('Deploy complete!');
-}, function(err) {
+}, err => {
 	console.error(err);
 	process.exit(1);
 });
