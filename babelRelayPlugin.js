81c967b6f35c14afb6420e697464a360e37995c7
Erik <erikdesjardins@users.noreply.github.com>
2016-03-17 23:51:41 -0400

    use ES6 module imports in babelRelayPlugin (because we can) no more whole-file `eslint-disable`, yay

--- babelRelayPlugin.js
+++ babelRelayPlugin.js
@@ -1,6 +1,4 @@
-/* eslint-disable */
+import getBabelRelayPlugin from 'babel-relay-plugin';
+import schema from './dist/schema.json'; // eslint-disable-line
 
-var getBabelRelayPlugin = require('babel-relay-plugin');
-var schema = require('./dist/schema.json');
-
-module.exports = getBabelRelayPlugin(schema.data);
+export default getBabelRelayPlugin(schema.data);
