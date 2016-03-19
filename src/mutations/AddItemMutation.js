f6f249976131c696bba3a5c7c365d89dd122b151
Erik <erikdesjardins@users.noreply.github.com>
2016-03-18 23:03:02 -0400

    complete implementation of AddItemMutation

--- src/mutations/AddItemMutation.js
+++ src/mutations/AddItemMutation.js
@@ -1,37 +1,38 @@
 import Relay from 'react-relay';
 
 export default class AddItemMutation extends Relay.Mutation {
-	static fragments = {
-		item: () => Relay.QL`
-			fragment on Item {
-				id,
-				date,
-			}
-		`,
-	};
-
 	getMutation() {
 		return Relay.QL`mutation { addItem }`;
+	}
+
+	getFatQuery() {
+		return Relay.QL`
+			fragment on AddItemPayload {
+				itemEdge
+			}
+		`;
 	}
 
 	getConfigs() {
 		return [{
 			type: 'RANGE_ADD',
-			parentName: 'TO BE IMPLEMENTED', // todo
-			parentID: 0, // todo
-			connectionName: 'items', // todo, sort of (probably will use this conn name)
-			edgeName: 'newItemEdge', // todo, sort of (ditto)
+			parentName: 'items',
+			parentID: 0,
+			connectionName: 'items',
+			edgeName: 'itemEdge',
 			rangeBehaviors: {
-				'': 'prepend',
+				'': 'append',
 			},
 		}];
 	}
 
-	getFatQuery() {
-		// todo whatever holds the items will need to have something updated
-		return Relay.QL`
-			fragment on AddItemPayload {
-			}
-		`;
+	getOptimisticResponse() {
+		return {
+			itemEdge: {
+				node: {
+					date: Date.now(),
+				},
+			},
+		};
 	}
 }
