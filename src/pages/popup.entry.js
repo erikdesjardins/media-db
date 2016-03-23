42c912ea466df624c278835fb61a0307cdfd6b2f
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2016-03-23 11:34:53 -0400

    rename and simplify ChromeNetworkLayer -> sendMessageNetworkLayer

--- src/pages/popup.entry.js
+++ src/pages/popup.entry.js
@@ -1,12 +1,12 @@
 import App from '../components/App';
-import ChromeNetworkLayer from '../network/ChromeNetworkLayer';
 import React from 'react';
 import Relay from 'react-relay';
+import sendMessageNetworkLayer from '../network/sendMessageNetworkLayer';
 import { Route, browserHistory } from 'react-router';
 import { RelayRouter } from 'react-router-relay';
 import { render } from 'react-dom';
 
-Relay.injectNetworkLayer(new ChromeNetworkLayer());
+Relay.injectNetworkLayer(sendMessageNetworkLayer);
 
 render((
 	<RelayRouter
