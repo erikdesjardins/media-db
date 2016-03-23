72aab93c4ef302868b7d39a41727ba867e2dd9f3
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2016-03-23 18:09:28 -0400

    use RelayRouter properly, have an IndexRedirect for popup.html

--- src/pages/index.entry.js
+++ src/pages/index.entry.js
@@ -2,35 +2,32 @@ import App from '../components/App';
 import React from 'react';
 import Relay from 'react-relay';
 import sendMessageNetworkLayer from '../network/sendMessageNetworkLayer';
-import { IndexRoute, Route, browserHistory } from 'react-router';
+import { IndexRedirect, IndexRoute, Route, browserHistory } from 'react-router';
 import { RelayRouter } from 'react-router-relay';
 import { render } from 'react-dom';
 
 Relay.injectNetworkLayer(sendMessageNetworkLayer);
 
 render((
-	<RelayRouter
-		history={browserHistory}
-		routes={
-			<Route
-				path="/" component={App}
-				queries={{
-					viewer: () => Relay.QL`query { viewer }`,
-				}}
-			>
-				<IndexRoute component={null}/>
-				<Route path="items" component={null}>
-					<Route path="items/:id" component={null}/>
-				</Route>
-				<Route path="items/by/:type/:query" component={null}>
-					<Route path="items/by/:type/:query/:id" component={null}/>
-				</Route>
-				<Route path="item/:id" component={null}/>
-				<Route path="random" component={null}/>
-				<Route path="tabs" component={null}/>
-				<Route path="storage" component={null}/>
-				<Route path="*" component={null}/>
+	<RelayRouter history={browserHistory}>
+		<Route
+			path="/" component={App}
+			queries={{
+				viewer: () => Relay.QL`query { viewer }`,
+			}}
+		>
+			<IndexRoute component={null}/>
+			<Route path="items" component={null}>
+				<Route path="items/:id" component={null}/>
 			</Route>
-		}
-	/>
+			<Route path="items/by/:type/:query" component={null}>
+				<Route path="items/by/:type/:query/:id" component={null}/>
+			</Route>
+			<Route path="item/:id" component={null}/>
+			<Route path="random" component={null}/>
+			<Route path="tabs" component={null}/>
+			<Route path="storage" component={null}/>
+			<Route path="*" component={null}/>
+		</Route>
+	</RelayRouter>
 ), document.getElementById('app'));
--- src/pages/popup.entry.js
+++ src/pages/popup.entry.js
@@ -2,22 +2,22 @@ import App from '../components/App';
 import React from 'react';
 import Relay from 'react-relay';
 import sendMessageNetworkLayer from '../network/sendMessageNetworkLayer';
-import { Route, browserHistory } from 'react-router';
+import { IndexRedirect, Route, browserHistory } from 'react-router';
 import { RelayRouter } from 'react-router-relay';
 import { render } from 'react-dom';
 
 Relay.injectNetworkLayer(sendMessageNetworkLayer);
 
 render((
-	<RelayRouter
-		history={browserHistory}
-		routes={
-			<Route
-				path="/popup.html" component={App}
-				queries={{
-					viewer: () => Relay.QL`query { viewer }`,
-				}}
-			/>
-		}
-	/>
+	<RelayRouter history={browserHistory}>
+		<Route
+			path="/" component={App}
+			queries={{
+				viewer: () => Relay.QL`query { viewer }`,
+			}}
+		/>
+		<Route path="/popup.html">
+			<IndexRedirect to="/"/>
+		</Route>
+	</RelayRouter>
 ), document.getElementById('app'));
