bcddeba44bcd2b868471feadb11699d8a1972b60
Erik <erikdesjardins@users.noreply.github.com>
2016-03-23 20:13:54 -0400

    actually ignore the correct rule `import/no-unresolved`

--- babelRelayPlugin.js
+++ babelRelayPlugin.js
@@ -1,4 +1,4 @@
 import getBabelRelayPlugin from 'babel-relay-plugin';
-import schema from './dist/schema.json'; // eslint-disable-line no-unresolved
+import schema from './dist/schema.json'; // eslint-disable-line import/no-unresolved
 
 export default getBabelRelayPlugin(schema.data);
