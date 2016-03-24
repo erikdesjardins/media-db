2bd621e9789ac3992688187e59d243e88289eca5
Erik <erikdesjardins@users.noreply.github.com>
2016-03-24 00:39:00 -0400

    basic navbar impl

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,16 @@ import React from 'react';
 import ReactCSS from 'reactcss';
 import packageName from 'prop?name!../../package.json';
 import icon from '../images/icon32.png';
-
-const indexPath = 'index.html';
+import { Input, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router';
 
 export default class Header extends ReactCSS.Component {
 	classes() {
 		return {
 			default: {
-				header: {
-					width: '100%',
-					backgroundColor: '#b689c8',
-					display: 'flex',
-					alignItems: 'center',
-				},
 				img: {
-					padding: '5px',
-				},
-				title: {
-					fontSize: 'large',
+					display: 'inline',
+					margin: '-6px 10px -6px 0',
 				},
 			},
 		};
@@ -27,18 +19,23 @@ export default class Header extends ReactCSS.Component {
 
 	render() {
 		return (
-			<div is="header">
-				<div is="img">
-					<a href={indexPath}>
-						<img src={icon}/>
-					</a>
-				</div>
-				<div is="title">
-					<a href={indexPath}>
-						{packageName}
-					</a>
-				</div>
-			</div>
+			<Navbar staticTop>
+				<Navbar.Header>
+					<Navbar.Brand>
+						<Link to="items">
+							<img is="img" src={icon}/>
+							{packageName}
+						</Link>
+					</Navbar.Brand>
+				</Navbar.Header>
+				<Nav>
+					<li><Link to="providers">{'Providers'}</Link></li>
+					<li><Link to="storage">{'Storage'}</Link></li>
+				</Nav>
+				<Navbar.Form pullRight>
+					<Input type="text" placeholder="Search"/>
+				</Navbar.Form>
+			</Navbar>
 		);
 	}
 }
