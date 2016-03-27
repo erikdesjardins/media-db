c262e081b4efd0d615aed986b63deeb2ca88ed05
Erik <erikdesjardins@users.noreply.github.com>
2016-03-26 23:41:12 -0400

    add react-overlays to make the sidebar info sticky

--- src/containers/Items.js
+++ src/containers/Items.js
@@ -2,6 +2,7 @@ import ItemList from '../components/ItemList';
 import React from 'react';
 import Relay from 'react-relay';
 import relay from 'relay-decorator';
+import { AutoAffix } from 'react-overlays';
 import { Col, Grid, Row } from 'react-bootstrap';
 
 @relay({
@@ -22,7 +23,11 @@ export default class Items extends React.Component {
 						<ItemList viewer={this.props.viewer}/>
 					</Col>
 					<Col xs={4}>
-						{this.props.children}
+						<AutoAffix viewportOffsetTop={15} container={this}>
+							<div>
+								{this.props.children}
+							</div>
+						</AutoAffix>
 					</Col>
 				</Row>
 			</Grid>
