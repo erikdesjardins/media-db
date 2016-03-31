b71eeebfea16524f1ffd1bab030b7939bde04cf7
Erik <erikdesjardins@users.noreply.github.com>
2016-03-31 19:05:49 -0400

    search: 100% width, 100% of the time

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -18,12 +18,7 @@ export default class Search extends React.Component {
 		return (
 			<Grid fluid>
 				<Row>
-					<Col
-						xs={12}
-						sm={12}
-						md={10} mdOffset={1}
-						lg={8} lgOffset={2}
-					>
+					<Col xs={12}>
 						<SearchList items={this.props.items}/>
 					</Col>
 				</Row>
