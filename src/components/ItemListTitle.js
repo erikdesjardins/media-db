eb260c06e31b6275fb4fdff41c7bd01890a31d78
Erik <erikdesjardins@users.noreply.github.com>
2016-04-02 15:24:37 -0400

    truncate item list titles and show the full title in the `title`

--- src/components/ItemListTitle.js
+++ src/components/ItemListTitle.js
@@ -17,9 +17,9 @@ import relay from 'relay-decorator';
 export default class ItemListTitle extends React.Component {
 	render() {
 		return (
-			<div className="CompactTable-item CompactTable-item--nowrap">
+			<div className="CompactTable-item CompactTable-item--nowrap CompactTable-item--truncate">
 				<Markdown
-					source={`[${this.props.item.title}](${this.props.item.url})${this.props.item.tags}`}
+					source={`[${this.props.item.title}](${this.props.item.url} "${this.props.item.title}")${this.props.item.tags}`}
 				/>
 			</div>
 		);
