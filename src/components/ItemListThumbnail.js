7046dc9501ca715024daa17ebe5223a3e5e64dd4
Erik <erikdesjardins@users.noreply.github.com>
2016-08-09 00:09:33 -0400

    move ItemListThumbnail inline styles to CSS in the name of performance

--- src/components/ItemListThumbnail.js
+++ src/components/ItemListThumbnail.js
@@ -13,18 +13,10 @@ import relay from 'relay-decorator';
 })
 export default class ItemListThumbnail extends React.Component {
 	render() {
-		const styles = {
-			thumbnail: {
-				maxHeight: '20px',
-				marginTop: '-8px',
-				marginBottom: '-6px',
-			},
-		};
-
 		return (
 			<div className="CompactTable-item">
 				{this.props.item.thumbnail &&
-					<img style={styles.thumbnail} src={this.props.item.thumbnail}/>
+					<img className="CompactTable-thumbnail" src={this.props.item.thumbnail}/>
 				}
 			</div>
 		);
