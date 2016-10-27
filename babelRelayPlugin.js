5d18511d1d2475293acea8aaa91dd6521a0854a5
Erik <erikdesjardins@users.noreply.github.com>
2016-10-27 12:46:44 -0400

    replace intermediate generation of schema.json for the Babel Relay plugin with a subprocess that generates it directly

--- babelRelayPlugin.js
+++ babelRelayPlugin.js
@@ -1,4 +1,9 @@
+import path from 'path';
+import { spawnSync } from 'child_process';
 import getBabelRelayPlugin from 'babel-relay-plugin';
-import schema from './dist/schema.json'; // eslint-disable-line import/no-unresolved
+
+const { stdout } = spawnSync(process.execPath, [path.join(__dirname, 'scripts', 'getSchema')], { encoding: 'utf8' });
+
+const schema = JSON.parse(stdout);
 
 export default getBabelRelayPlugin(schema.data);
