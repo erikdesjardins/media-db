e467402140e7457dca048eaa22748c03e0aa2211
Erik <erikdesjardins@users.noreply.github.com>
2018-01-02 22:40:14 -0500

    avoid using dangerouslySetInnerHTML when possible
    This gives a solid (>2x) improvement when rendering everything on a search page [with no queries].
    The improvement is in reconciliation (avoiding .innerHTML inside React), not from avoiding the snudown render call, which only accounted for ~1%.

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -2,18 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { markdown } from 'snudown-js';
 
+const isMarkdown = /[*_~^[&#]/;
+
 export default class Markdown extends React.PureComponent {
 	static propTypes = {
 		source: PropTypes.string.isRequired,
 	};
 
-	renderHtml() {
-		return { __html: markdown(this.props.source) };
-	}
-
 	render() {
-		return (
-			<div dangerouslySetInnerHTML={this.renderHtml()}/> // eslint-disable-line react/no-danger
+		return (isMarkdown.test(this.props.source) ?
+			<div dangerouslySetInnerHTML={{ __html: markdown(this.props.source) }}/> : // eslint-disable-line react/no-danger
+			<div><p>{this.props.source}</p></div>
 		);
 	}
 }
