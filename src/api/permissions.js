6369f865e21fd8e5b03c2dbce3a76e8bc7c1d77b
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2021-10-15 20:40:57 -0400

    add note about why permissions requests are disabled

--- src/api/permissions.js
+++ src/api/permissions.js
@@ -1,5 +1,10 @@
 import { apiToPromise } from '../utils/api';
 
+// may have to wait for https://groups.google.com/a/chromium.org/g/chromium-extensions/c/EnUmtHWOI9o to enable this,
+// since you currently can't have optional host permissions
+// "host_permissions": [
+// 	 "https://*/*"
+// ],
 export async function requestPermissionForUrl(url) {
 	const perms = { origins: [url] };
 
