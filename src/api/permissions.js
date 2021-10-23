4951f0378787e7f41c532db0a9f18c3ecb4b4301
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2021-10-22 22:51:07 -0400

    remove now-unnecessary apiToPromise

--- src/api/permissions.js
+++ src/api/permissions.js
@@ -1,5 +1,3 @@
-import { apiToPromise } from '../utils/api';
-
 // may have to wait for https://groups.google.com/a/chromium.org/g/chromium-extensions/c/EnUmtHWOI9o to enable this,
 // since you currently can't have optional host permissions
 // "host_permissions": [
@@ -8,10 +6,10 @@ import { apiToPromise } from '../utils/api';
 export async function requestPermissionForUrl(url) {
 	const perms = { origins: [url] };
 
-	const alreadyHave = await apiToPromise(chrome.permissions.contains)(perms);
+	const alreadyHave = await chrome.permissions.contains(perms);
 	if (alreadyHave) {
 		return;
 	}
 
-	await apiToPromise(chrome.permissions.request)(perms);
+	await chrome.permissions.request(perms);
 }
