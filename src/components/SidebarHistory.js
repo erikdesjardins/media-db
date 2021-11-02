878d8f19126d39c2aaa15acf0b9f272cfd714a68
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2021-11-01 21:59:24 -0400

    use keepPreviousData for queries providing data to sidebar
    this saves one rerender (to blank) when switching from one selection to another

--- src/components/SidebarHistory.js
+++ src/components/SidebarHistory.js
@@ -2,7 +2,7 @@ import ItemHistory from './ItemHistory';
 import { useQueryItemHistory } from '../data/queries';
 
 export default function SidebarHistory({ itemId: id }) {
-	const { isLoading, data: history } = useQueryItemHistory(id);
+	const { isLoading, data: history } = useQueryItemHistory(id, { keepPreviousData: true });
 
 	if (isLoading) {
 		return null;
--- src/components/SidebarInfo.js
+++ src/components/SidebarInfo.js
@@ -2,7 +2,7 @@ import ItemInfo from './ItemInfo';
 import { useQueryItem } from '../data/queries';
 
 export default function SidebarInfo({ itemId: id }) {
-	const { isLoading, data: item } = useQueryItem(id);
+	const { isLoading, data: item } = useQueryItem(id, { keepPreviousData: true });
 
 	if (isLoading) {
 		return null;
