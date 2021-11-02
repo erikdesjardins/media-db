1279ec6f60a2e4366a8ff8a5ef5295642f22a207
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2021-11-01 21:45:47 -0400

    handle search errors

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -3,13 +3,21 @@ import { ROW_LIMIT } from '../constants/table';
 import { useQueryItemsSearch } from '../data/queries';
 
 export default function SearchList({ query, preview = false, onClickItem }) {
-	const { isLoading, data: items } = useQueryItemsSearch(query, { keepPreviousData: true });
+	const { isLoading, isError, error, data: items } = useQueryItemsSearch(query, { keepPreviousData: true });
+
+	const limit = preview ? ROW_LIMIT : 2147483647;
 
 	if (isLoading) {
 		return null;
 	}
 
-	const limit = preview ? ROW_LIMIT : 2147483647;
+	if (isError) {
+		return (
+			<fieldset className="SearchList">
+				{'❌'}{' '}{error.message}
+			</fieldset>
+		);
+	}
 
 	return (
 		<fieldset className="SearchList Utils-fieldset--noPadding">
