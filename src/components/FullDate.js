fe0371e0085d635dc5f25db45beebc8a27bd940d
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2022-01-02 23:32:44 -0500

    correctly use "second(s)" in date tooltip
    it's the little things.

--- src/components/FullDate.js
+++ src/components/FullDate.js
@@ -2,10 +2,11 @@ import { formatFullDate } from '../utils/date';
 import React from 'react';
 
 export default React.memo(function FullDate({ date }) {
+	const seconds = new Date(date).getSeconds();
 	return (
 		<span
 			className="FullDate"
-			title={`...and ${new Date(date).getSeconds()} seconds`}
+			title={`...and ${seconds} ${seconds === 1 ? 'second' : 'seconds'}`}
 		>
 			{formatFullDate(date)}
 		</span>
