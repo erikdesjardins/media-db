ec1cc7c0f337ef4f2eddb5d6bf8901a694747b0a
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2022-01-02 21:39:17 -0500

    remove special case for Date objects in structuralEq
    it doesn't really match the "theme" of the function, and we don't ever use it, since we store dates as numeric timestamps

--- src/utils/object.js
+++ src/utils/object.js
@@ -8,10 +8,11 @@ export function property(obj, path) {
 }
 
 // Compare two objects structurally.
-// Recursively compares own properties using strict equality*.
-// Does not check prototypes*.
+// Recursively compares own properties using strict equality.
+// Does not check prototypes.
 // Does not handle cyclic references.
-// * With the exception of `Date` objects, which are treated like a primitive type (as if `typeof === 'date'`).
+// Due to this, objects with zero own properties will compare equal to empty objects and each other.
+// For example, all Date, Number, String, etc. objects will compare equal.
 export function structuralEq(a, b) {
 	// 1. Strict equality
 	if (a === b) {
@@ -25,14 +26,10 @@ export function structuralEq(a, b) {
 	if (typeof a !== 'object' || typeof b !== 'object') {
 		return false;
 	}
-	// 4. Handle `Date`
-	if (a instanceof Date || b instanceof Date) {
-		return a instanceof Date && b instanceof Date && a.valueOf() === b.valueOf();
-	}
-	// 5. Compare objects
+	// 4. Compare objects
 	const aKeys = Object.keys(a);
 	const bKeys = Object.keys(b);
-	// 5.1. Exclude different key counts
+	// 4.1. Exclude different key counts
 	if (aKeys.length !== bKeys.length) {
 		return false;
 	}
@@ -41,11 +38,11 @@ export function structuralEq(a, b) {
 	for (let i = 0; i < aKeys.length; ++i) {
 		const aKey = aKeys[i];
 		const bKey = bKeys[i];
-		// 5.2. Exclude different key names
+		// 4.2. Exclude different key names
 		if (aKey !== bKey) {
 			return false;
 		}
-		// 5.3. Recurse onto values
+		// 4.3. Recurse onto values
 		if (!structuralEq(a[aKey], b[aKey])) {
 			return false;
 		}
