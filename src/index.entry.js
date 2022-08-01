b8bde09c1e64718e38f1298e6de747d52b6dad51
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2022-08-01 11:10:59 -0400

    update deps, in particular react-query
    (for React 18 support, e.g. useSyncExternalStore and concurrent mode support)

--- src/index.entry.js
+++ src/index.entry.js
@@ -1,5 +1,5 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
-import { QueryClientProvider } from 'react-query';
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Header from './containers/Header';
 import History from './containers/History';
